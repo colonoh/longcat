78a3fd0380d347f014655019885b2132d64a05d7
colonoh <steve.j.warner@gmail.com>
2020-04-16 19:52:41 -0700

    Cleaned up comments

--- sluggen/views.py
+++ sluggen/views.py
@@ -1,26 +1,14 @@
-# from django.shortcuts import render
-
 from rest_framework import generics
 
 from sluggen.models import Slug
 from sluggen.serializers import SlugListSerializer, SlugCreateSerializer
 
 
-# POST
-# create a new slug from a URL
 class SlugCreate(generics.CreateAPIView):
     '''
     Create a new slug
     '''
     serializer_class = SlugCreateSerializer
-
-    # def create(self, request):
-    #     print('setting slug')
-    #     self.slug = 'abc'
-    
-    # def perform_create(self, serializer):
-    #     print('Performing create!!!')
-    #     serializer.save()
 
 class SlugList(generics.ListAPIView):
     '''
@@ -28,6 +16,3 @@ class SlugList(generics.ListAPIView):
     '''
     queryset = Slug.objects.all()
     serializer_class = SlugListSerializer
-
-# GET
-# read a URL for a pecific slug
